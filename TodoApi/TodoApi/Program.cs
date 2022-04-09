da6c1a9fcc3dfb6fb6ce872fa99477a630555ac2
joelee <as955133@gmail.com>
2022-04-09 17:47:21 +0800

    dfasdf

--- TodoApi/TodoApi/Program.cs
+++ TodoApi/TodoApi/Program.cs
@@ -23,7 +23,17 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-var summaries2 = new[]
+var summaries3 = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+var summaries4 = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+var summaries5 = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
